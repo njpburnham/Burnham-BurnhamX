0f06dc4f707a9f3a5c2797c312f51613db950db3
Alex Voorhees <alex@cloudbakers.com>
2015-05-11 16:26:58 -0500

    he's migrated! BURX-10 #resolve

--- migrate.py
+++ migrate.py
@@ -1,16 +1,31 @@
+# -*- coding: utf-8 -*-
 import os, sys
-os.environ['DJANGO_SETTINGS_MODULE'] = 'burnhmax.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'burnhamx.settings'
 from django.conf import settings
-from extension.models import Opportunity, Associationfrom datetime import date, timedelta, datetime
+from extension.models import Opportunity, Association, Users
+from datetime import date, timedelta, datetime
 
 import pprint
 
-
-# billing_type = models.ForeignKey("BillingType")
-# sale_price = models.DecimalField(max_digits=6, decimal_places=2)
-# purch_price = models.DecimalField(max_digits=6, decimal_places=2)
-# start_date = models.DateField()
-# end_date = models.DateField()
-
-
 import csv
+
+
+
+with open("carson.csv", "rU") as csv_file:
+  reader = csv.DictReader(csv_file)
+  for row in reader:
+    curr_opp = Opportunity.objects.filter(siebel_id=row['Opty'])
+    user = Users.objects.filter(email=row['Email'])
+    if not user:
+      user = Users()
+      user.email = row['Email']
+      user.save()
+    else:
+      user = user[0]
+    if curr_opp:
+      user.opportunities.add(curr_opp[0])
+      print "Added: %s" % curr_opp[0]
+    else:
+      pprint.pprint(row['Opty'])
+
+
